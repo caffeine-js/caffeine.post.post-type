dfd455289839f54a813447fd519cef4f129738d8
Alan Reis <alanreisanjo@gmail.com>
2026-01-29 15:37:08 -0300

    Melhorias no Cache

--- src/infra/repositories/cached/post-type.repository.ts
+++ src/infra/repositories/cached/post-type.repository.ts
@@ -1,7 +1,7 @@
 import type { PostType } from "@/domain/post-type";
 import type { IPostTypeRepository } from "@/domain/types/post-type-repository.interface";
 import type { IUnmountedPostType } from "@/domain/types/unmounted-post-type.interface";
-import { redis } from "bun";
+import { redis } from "@caffeine/redis-drive";
 
 export class PostTypeRepository implements IPostTypeRepository {
 	private postTypeCacheExpirationTime: number = 60 * 60;
@@ -20,8 +20,16 @@ export class PostTypeRepository implements IPostTypeRepository {
 
 		const targetPostType = await this.repository.findById(id);
 
+		if (!targetPostType) return null;
+
 		await redis.set(
 			`post@post-type::$${id}`,
+			JSON.stringify(targetPostType),
+			"EX",
+			this.postTypeCacheExpirationTime,
+		);
+		await redis.set(
+			`post@post-type::${targetPostType?.slug}`,
 			JSON.stringify(targetPostType),
 			"EX",
 			this.postTypeCacheExpirationTime,
@@ -38,8 +46,16 @@ export class PostTypeRepository implements IPostTypeRepository {
 
 		const targetPostType = await this.repository.findBySlug(slug);
 
+		if (!targetPostType) return null;
+
 		await redis.set(
 			`post@post-type::${slug}`,
+			JSON.stringify(targetPostType),
+			"EX",
+			this.postTypeCacheExpirationTime,
+		);
+		await redis.set(
+			`post@post-type::$${targetPostType?.id}`,
 			JSON.stringify(targetPostType),
 			"EX",
 			this.postTypeCacheExpirationTime,
@@ -66,8 +82,14 @@ export class PostTypeRepository implements IPostTypeRepository {
 	}
 
 	async update(postType: PostType): Promise<void> {
-		await redis.del(`post@post-type::$${postType.id}`);
-		await redis.del(`post@post-type::${postType.slug}`);
+		const _cachedPostType = await redis.get(`post@post-type::$${postType.id}`);
+
+		if (_cachedPostType) {
+			const cachedPostType: IUnmountedPostType = JSON.parse(_cachedPostType);
+
+			await redis.del(`post@post-type::$${cachedPostType.id}`);
+			await redis.del(`post@post-type::${cachedPostType.slug}`);
+		}
 
 		await this.repository.update(postType);
 
